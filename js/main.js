ba7431d598afb105de585ba975f401ea31b9d344
BrunoAdriel <109391716+BrunoAdriel@users.noreply.github.com>
2022-09-24 20:40:47 -0300

    se agrega el proyecto

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// let NombreGuardado = "Adriel";
+// let ConstraseñaGuardad = "1217";
+
+// function login(){
+//     let inicio = false;
+//     for (let i = 3; i=0; i-- ) {
+//         let NombreUsuario = Prompt(
+//             "Ingrese su nombre de usuario, tenes" + i + " oportunidades"
+//         );
+//         let Contraseña = Prompt(
+//             "Ingrese su contraseña, tenes" + i + " oportunidades"
+//         );
+//     if (NombreUsuario === NombreGuardado, ConstraseñaGuardad === Contraseña){
+//         alert("Bienvenido" + NombreGuardado + "puedes seguir comprando");
+//         let inicio = true;
+//         break;
+//     }else {
+//         alert("Usuario o Contraseña incorrecto, puedes seguir usando la pagina sin comprar")
+//     }
+//     }
+
+//     return ingresar;
+// }
+// login();
+
+// let NombreGuardado = "Adriel";
+// function login() {
+//     let inicio = false;
+//     for ( let i = 3; i=0; i--){
+//         let NombreUsuario = prompt(
+//             "Ingresa tu nombre"
+//         );
+//     if (NombreUsuario === NombreGuardado) {
+//         alert ("hola")
+//         let inicio= true;
+//         break;
+//     }else{
+//         alert("Usuario o Contraseña incorrecto, puedes seguir usando la pagina sin comprar");
+//     }
+//     }
+// }
+// login();
+
+let NombreGuardado = "Adriel";
+function login() {
+    let inicio = false;
+    for ( let i = 3; i=0; i--){
+        let NombreUsuario = prompt(
+            "Ingresa tu nombre"
+        );
+    };
+}
+login();
