d9bdf1fff9c8c0fffee1fcf0b5d75d09e1e3b25d
BrunoAdriel <109391716+BrunoAdriel@users.noreply.github.com>
2022-10-23 23:18:47 -0300

    agrego de paginas ingreso y registro

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-const tbody = document.querySelector("#lista")
-
 // Inicio con usuario guardado
 
 // let usuario = prompt("Ingresa tu nombre");
@@ -128,7 +126,7 @@ const producto =[
 function prods(id, nombre, precio, color, talle, tipo, genero, img, stock){
     this.id = id;
     this.nombre = nombre;
-    this.precio = precio;
+    this.precio = parseFloat(precio);
     this.color = color;
     this.talle = talle;
     this.tipo = tipo;
@@ -205,7 +203,6 @@ function cargarInventario(arr, prods){
 
 function crearHtml(arr){
     div.innerHTML = ""
-
         let html = "";
         for (const producto of arr) {
             html = `
@@ -221,7 +218,73 @@ function crearHtml(arr){
 }
 
 cargarInventario(producto);
-crearHtml(producto);
+// crearHtml(producto);
 
-// cargarInventario(tbod);
-// 0:36::54
+
+// ------------- ------------- ------------- funcionces de inicio de sesion ------------- ------------- -------------
+// constantes
+
+const   nombre = document.querySelector("#nombre"),
+        apellido = document.querySelector("#apellido"),
+        email = document.querySelector("#email"),
+        pass = document.querySelector("#pass"),
+        btnR =document.querySelector ("#BtnR")
+
+
+// clase y funciones principales
+let usuarios;
+if (localStorage.getItem("usuarios")){
+    usuarios = JSON.parse(localStorage.getItem("usuarios"))
+}else {
+    usuarios = [];
+}
+
+
+class usuario{
+    constructor(nombre, apellido, email, pass){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.pass = pass;
+    }
+}
+
+function limpiar(){
+    nombre.value = "";
+    apellido.value = "";
+    email.value = "";
+    pass.value = "";
+}
+
+function guardarUsuario(usuario){
+    return usuarios.push(usuario)
+}
+
+btnR.addEventListener("click", (e) =>{
+    e.preventDefault();
+    let nuevoUser = new usuario(
+        nombre.value,
+        apellido.value,
+        email.value,
+        pass.value
+    );
+    
+
+    guardarUsuario(nuevoUser);
+    limpiar();
+    guardarEnLocal(usuarios);
+    
+    })
+
+// funciones Storage
+
+function guardarEnLocal(el){
+    return localStorage.setItem("usuarios", JSON.stringify(el) )
+}
+
+
+// function InicioSecion(usuarios){
+//     let userFound = usuario.find (usuario) =>(
+//         return usuario.nombre === .value
+//     )
+// }
